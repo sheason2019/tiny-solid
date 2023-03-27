39dd32b2205c21130125fc3aaf5db4b101617aeb
sheason2019 <811627877@qq.com>
2023-03-27 18:17:05 +0800

    feat: git init

---
+++ package/src/jsx-runtime/index.ts
@@ -0,0 +1,3 @@
+export const jsx = (label: string) => {
+  console.log('create label', label);
+};
---
+++ test/src/index.tsx
@@ -0,0 +1,8 @@
+import { h } from "tiny-solid";
+
+const App = () => {
+  return <div>123</div>;
+};
+
+App();
+export default App;
