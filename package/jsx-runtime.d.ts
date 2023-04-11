43d4c402e3c8ca6c69233955d6d9f11c35e9b452
sheason2019 <811627877@qq.com>
2023-04-11 11:12:19 +0800

    feat: 修正Demo样式

--- package/jsx-runtime.d.ts
+++ package/jsx-runtime.d.ts
@@ -1,7 +1,19 @@
 export namespace JSX {
+  interface ElementBase {
+    class?: string;
+  }
+
+  interface HTMLDivElement extends ElementBase {
+    children?: any;
+  }
+  interface HTMLButtonElement extends ElementBase {
+    onclick?: () => () => any;
+  }
+  interface HTMLSpanElement extends ElementBase {}
+
   interface IntrinsicElements {
-    div: { children?: any };
-    button: { onclick: () => () => any };
-    span: {};
+    div: HTMLDivElement;
+    button: HTMLButtonElement;
+    span: HTMLSpanElement;
   }
 }
--- test-app/src/main.tsx
+++ test-app/src/main.tsx
@@ -1,8 +1,10 @@
 import { h, createSignal, createMemo, createEffect } from "tiny-solid";
 
-const App = () => {
-  const [count, setCount] = createSignal<number>(0);
+import "./main.css";
 
+const [count, setCount] = createSignal<number>(0);
+
+const App = () => {
   const doubleCount = createMemo(() => count() * 2);
 
   createEffect(() => {
@@ -10,12 +12,24 @@ const App = () => {
   });
 
   return (
-    <div>
-      <div>{() => count()}</div>
-      <button onclick={() => () => setCount(count() + 1)}>increment</button>
-      <div>
+    <div class="root-container">
+      <div class="h1">Counter Example</div>
+      <CounterShower />
+      <button class="button" onclick={() => () => setCount(count() + 1)}>
+        Count++
+      </button>
+      <div class="double-count">
         double count <span>{() => doubleCount()}</span>
       </div>
+    </div>
+  );
+};
+
+const CounterShower = () => {
+  return (
+    <div class="counter-shower">
+      <span>跨组件响应性示例：</span>
+      <span>{() => count()}</span>
     </div>
   );
 };
